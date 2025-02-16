fae4b5c5863f4547a8fed88962a75387308ff9d9
andersbor <anbo@zealand.dk>
2025-02-16 11:53:52 +0100

    steps update

--- app/src/main/java/com/example/sliderexample/MainActivity.kt
+++ app/src/main/java/com/example/sliderexample/MainActivity.kt
@@ -9,15 +9,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.sliderexample.ui.theme.SliderExampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,11 +37,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun SliderSample(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
         var sliderPosition by remember { mutableFloatStateOf(0f) }
         Slider(
             value = sliderPosition,
             onValueChange = { sliderPosition = it },
+            valueRange = 0f..100f,
+            steps = 9, // number of inner points
+            colors = SliderDefaults.colors(
+                thumbColor = SliderDefaults.colors().thumbColor.copy(alpha = 0.5f)
+            )
         )
         Text(text = "Slider position: $sliderPosition")
     }
